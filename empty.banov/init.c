a8418b13d6b08b7303dacedb669851c4f939387c
Banov <87153569+KubeloLive@users.noreply.github.com>
2024-03-11 16:45:10 +0100

    Add files via upload

--- empty.banov/init.c
+++ empty.banov/init.c
@@ -8,8 +8,8 @@ void main()
 	GetHive().InitOffline();
 	
 //Loot spawn Creator
-GetCEApi().ExportProxyData(vector.Zero, 100000); //Loot
-GetCEApi().ExportClusterData(); //Fruit	
+//GetCEApi().ExportProxyData(vector.Zero, 100000); //Loot
+//GetCEApi().ExportClusterData(); //Fruit	
 
 	//DATE RESET AFTER ECONOMY INIT-------------------------
 	int year, month, day, hour, minute;
